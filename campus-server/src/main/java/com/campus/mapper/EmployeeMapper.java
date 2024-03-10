0ad82651ca86d2269c6b8b1b15d6fcfa186c6c98
ldx <1113756394@qq.com>
2024-03-10 17:32:59 +0800

    更改

---
+++ campus-server/src/main/java/com/campus/mapper/EmployeeMapper.java
@@ -0,0 +1,50 @@
+package com.campus.mapper;
+
+import com.github.pagehelper.Page;
+import com.campus.dto.EmployeePageQueryDTO;
+import com.campus.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    /**
+     * 新增员工
+     * @param employee
+     */
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
+            "values " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    void save(Employee employee);
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 更新员工表
+     * @param employee
+     */
+    void update(Employee employee);
+
+    /**
+     * 根据Id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Integer id);
+}
